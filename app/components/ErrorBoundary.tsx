0a4e1fc10a8e63c32bda05587c95d9895a9b97b6
Jessica <isaians.jessica@gmail.com>
2024-10-18 20:07:08 +0330

    added error boundary for test

--- app/components/ErrorBoundary.tsx
+++ app/components/ErrorBoundary.tsx
@@ -1,22 +1,35 @@
 "use client";
-import React from "react";
+import React, { ReactNode, ErrorInfo } from "react";
 
-class ErrorBoundary extends React.Component {
-  constructor(props) {
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
     super(props);
 
-    // Define a state variable to track whether is an error or not
+    // Define a state variable to track whether there is an error or not
     this.state = { hasError: false };
   }
-  static getDerivedStateFromError(error) {
-    // Update state so the next render will show the fallback UI
 
+  static getDerivedStateFromError(_: Error): ErrorBoundaryState {
+    // Update state so the next render will show the fallback UI
     return { hasError: true };
   }
-  componentDidCatch(error, errorInfo) {
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
     // You can use your own error logging service here
     console.log({ error, errorInfo });
   }
+
   render() {
     // Check if the error is thrown
     if (this.state.hasError) {
@@ -35,7 +48,6 @@ class ErrorBoundary extends React.Component {
     }
 
     // Return children components in case of no error
-
     return this.props.children;
   }
 }
